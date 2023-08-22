37c9f4286348401f7c9191387d249fada578002b
vov4ik <Event88888@gmail.com>
2023-08-23 01:12:35 +0300

    proba #3

--- lab3/tasks_1/proba.py
+++ lab3/tasks_1/proba.py
@@ -1,2 +1,20 @@
+ROWS = 5
+COLS = 3
+
 def main():
-    pass
+
+    numbers = []
+
+    for i in range(ROWS):
+        numbers.append([])
+        for g in range(COLS):
+            numbers[i].append(0)
+
+    for nums in range(ROWS):
+        for num in range(COLS):
+            numbers[nums][num] = int(input("Введите случайное число - "))
+
+    print(numbers)
+
+if __name__ == '__main__':
+    main()
